0d53b33fdada3d1e82521a21670ff5d1b660dac8
st <storm_ne@inbox.ru>
2022-12-06 23:35:59 +0300

    уже приличнее=)

--- nomenclature/nomenclature_router.py
+++ nomenclature/nomenclature_router.py
@@ -2,25 +2,15 @@ from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from . import crud, schemas
 from db.database import get_db
-from auth.util import get_user_role, get_role_user, get_role_manager, permission_req
+from auth.util import get_user_role, permission_req
 
 nomenclature_router = APIRouter(prefix="", dependencies=[Depends(get_db)],
                                 responses={404: {"description": "Not found"}})
 
 
 @nomenclature_router.get("/category", response_model=list[schemas.Category])
-async def read_categories(db: Session = Depends(get_db), role=Depends(get_user_role), roles=('manager', 'user')):
-    for i in roles:
-        if role == i:  # мне не нравится это, н что-то пока не получается по нормальному=(
-            return await crud.get_categories(db)  # хотя оно работает. ручки кривые сделать "красиво" =(
-    raise HTTPException(status_code=403, detail="Forbidden")
-
-
-@nomenclature_router.get("/category_test", response_model=list[schemas.Category])
-async def read_categories_test(db: Session = Depends(get_db), permissions=('manager', 'admin'), role=Depends(get_role_user)):
-    if await permission_req(role, permissions):
-        return await crud.get_categories(db)  # с 1 зависимостью работает, с 2 нет(
-    raise HTTPException(status_code=403, detail="Forbidden")
+async def read_categories(db: Session = Depends(get_db)):
+    return await crud.get_categories(db)
 
 
 @nomenclature_router.get("/category/{title}", response_model=list[schemas.Nomenclature])
@@ -29,8 +19,13 @@ async def get_category_products(category: str, db: Session = Depends(get_db)):
 
 
 @nomenclature_router.post("/category", response_model=schemas.Category)
-async def create_category(category: schemas.CategoryCreate, db: Session = Depends(get_db)):
-    return await crud.create_category(db=db, category=category)
+async def create_category(category: schemas.CategoryCreate,
+                          db: Session = Depends(get_db),
+                          permissions=('admin', 'manager'),
+                          role=Depends(get_user_role)):
+    if await permission_req(role, permissions):
+        return await crud.create_category(db=db, category=category)
+    raise HTTPException(status_code=403, detail="Forbidden")
 
 
 @nomenclature_router.get('/nomenclature', response_model=list[schemas.Nomenclature])
@@ -39,22 +34,22 @@ async def read_nomenclatures(db: Session = Depends(get_db)):
     return nomenclatures
 
 
-@nomenclature_router.post("/nomenclature", response_model=schemas.Nomenclature)
-async def create_nomenclature(nomenclature: schemas.NomenclatureCreate, db: Session = Depends(get_db)):
-    # db_nomenclatures = await crud.get_nomenclature_by_product(db, product=nomenclature.product)
-    # if db_nomenclatures:
-    #    raise HTTPException(status_code=400, detail="ТАкое уже есть, надо добавлять кол-во")
-    return await crud.create_nomenclature(db=db, nomenclature=nomenclature)
-
-
 @nomenclature_router.post("/nomenclatures")
-async def create_nomenclatures(nomenclatures: list[schemas.NomenclatureCreate], db: Session = Depends(get_db)):
-    for i in nomenclatures:
-        await crud.create_nomenclatures(db=db,
-                                        product=i.product,
-                                        description=i.description,
-                                        remainder=i.remainder,
-                                        category_title=i.category_title,
-                                        price=i.price)
-
-    return nomenclatures
+async def create_nomenclatures(nomenclatures: list[schemas.NomenclatureCreate],
+                               db: Session = Depends(get_db),
+                               permissions=('admin', 'manager'),
+                               role=Depends(get_user_role)):
+    if await permission_req(role, permissions):
+        for i in nomenclatures:
+            db_nomenclatures = await crud.get_nomenclature_by_product(db=db, product=i.product)
+            if db_nomenclatures:
+                raise HTTPException(status_code=400,
+                                    detail="ТАкое уже есть, надо добавлять кол-во")
+            await crud.create_nomenclatures(db=db,
+                                            product=i.product,
+                                            description=i.description,
+                                            remainder=i.remainder,
+                                            category_title=i.category_title,
+                                            price=i.price)
+        return nomenclatures
+    raise HTTPException(status_code=403, detail="Forbidden")
--- users/users_routers.py
+++ users/users_routers.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter
 from auth.util import *
 from db.database import get_db
 from . import schemas, crud
@@ -8,6 +8,9 @@ user_router = APIRouter(
     dependencies=[Depends(get_db)],
     responses={404: {"description": "Not found"}},
 )
+permission = ('admin', 'manager')
+roles = Depends(get_user_role)
+
 
 
 @user_router.get("/user/profile/")
@@ -17,9 +20,14 @@ async def read_users_me(current_user: User = Depends(get_current_active_user)):
 
 
 @user_router.get("/users", response_model=list[schemas.User])
-async def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    users = await crud.get_users(db, skip=skip, limit=limit)
-    return users
+async def read_users(skip: int = 0, limit: int = 100,
+                     db: Session = Depends(get_db),
+                     permissions=permission,
+                     role=roles):
+    if await permission_req(role, permissions):
+        users = await crud.get_users(db, skip=skip, limit=limit)
+        return users
+    raise HTTPException(status_code=403, detail="Forbidden")
 
 
 @user_router.post("/users", response_model=schemas.User)
@@ -31,16 +39,20 @@ async def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
 
 
 @user_router.get("/user/{username}", response_model=schemas.User)
-async def read_user_username(username: str, db: Session = Depends(get_db)):
-    db_user = crud.get_user_by_username(db, username=username)
-    if db_user is None:
-        raise HTTPException(status_code=404, detail="User not found")
-    return db_user
+async def read_user_username(username: str, db: Session = Depends(get_db), permissions=permission, role=roles):
+    if await permission_req(role, permissions):
+        db_user = crud.get_user_by_username(db, username=username)
+        if db_user is None:
+            raise HTTPException(status_code=404, detail="User not found")
+        return db_user
+    raise HTTPException(status_code=403, detail="Forbidden")
 
 
 @user_router.get("/user/id/{user_id}", response_model=schemas.User)
-async def read_user(user_id: int, db: Session = Depends(get_db)):
-    db_user = await crud.get_user_by_id(db, user_id=user_id)
-    if db_user is None:
-        raise HTTPException(status_code=404, detail="User not found")
-    return db_user
+async def read_user(user_id: int, db: Session = Depends(get_db), permissions=permission, role=roles):
+    if await permission_req(role, permissions):
+        db_user = await crud.get_user_by_id(db, user_id=user_id)
+        if db_user is None:
+            raise HTTPException(status_code=404, detail="User not found")
+        return db_user
+    raise HTTPException(status_code=403, detail="Forbidden")
